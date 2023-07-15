37a9209f4b714c3d25118f83e4aafb2668a619b9
utube2 <test@test.com>
2023-07-15 12:29:00 +0530

    eight

--- deploy_k8/server.js
+++ deploy_k8/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">VICTOR FORCE - GLOBAL App - VICTORFORCE HITS @#@ -10000000000 !!</h1> \n');});
+  res.send('<h1 style="color:green;">BULU FORCE - GLOBAL App - SIBSAGAR HITS @#@ -10000000000 !!</h1> \n');});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
